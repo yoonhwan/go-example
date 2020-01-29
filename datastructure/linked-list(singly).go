ce719f85ef9bea5afc84b58b72b8a0ccc6150b44
yoonhwan.ko <yoonhwan.ko@neowiz.com>
2020-01-29 11:33:53 +0900

    make singly linked list

---
+++ datastructure/linked-list(singly).go
@@ -0,0 +1,185 @@
+package linkedlist
+
+import "fmt"
+
+var (
+	head *SNode
+)
+
+type SNode struct {
+	Value int
+	next  *SNode
+}
+
+func (this *SNode) set(value int) {
+	this.Value = value
+}
+
+func (this *SNode) get() int {
+	return this.Value
+}
+
+func (this *SNode) setNext(node *SNode) *SNode {
+	this.next = node
+	return node
+}
+
+func (this *SNode) getNext() *SNode {
+	return this.next
+}
+
+func (this *SNode) clear() {
+	this.Value = 0
+	this.next = nil
+}
+
+func MakeNewNode(value int) *SNode {
+	node := new(SNode)
+	(*node).set(value)
+	return node
+}
+
+func MakeNext(value int) *SNode {
+	if head == nil {
+		panic("")
+	}
+
+	var node *SNode = head
+outer:
+	for {
+		if (*node).getNext() == nil {
+			node = (*node).setNext(MakeNewNode(value))
+			break outer
+		} else {
+			node = (*node).getNext()
+		}
+	}
+
+	return node
+}
+
+type excute func(int)
+
+func Tour(fn excute) {
+	if head == nil {
+		panic("")
+	}
+
+	var node *SNode = head
+outer:
+	for {
+		fn((*node).get())
+		if (*node).getNext() == nil {
+			break outer
+		} else {
+			node = (*node).getNext()
+		}
+	}
+}
+
+func Len() int {
+	if head == nil {
+		panic("")
+	}
+
+	var node *SNode = head
+	var length int = 0
+outer:
+	for {
+		if node != nil {
+			length++
+		}
+		if (*node).getNext() == nil {
+			break outer
+		} else {
+			node = (*node).getNext()
+		}
+	}
+
+	return length
+}
+
+func Clear() {
+	if head == nil {
+		panic("")
+	}
+
+	var node *SNode = head
+	var prev *SNode = nil
+outer:
+	for {
+		if Len() == 1 {
+			break outer
+		}
+
+		if (*node).getNext() == nil {
+			prev.clear()
+			node = head
+		} else {
+			prev = node
+			node = (*node).getNext()
+		}
+	}
+}
+
+func InsertNext(target int, value int) {
+	if head == nil {
+		panic("")
+	}
+
+	var node *SNode = head
+outer:
+	for {
+		if (*node).get() == target {
+			newone := MakeNewNode(value)
+			(*newone).setNext((*node).getNext())
+			(*node).setNext(newone)
+			break outer
+		}
+
+		if (*node).getNext() == nil {
+			break outer
+		} else {
+			node = (*node).getNext()
+		}
+	}
+}
+
+func Delete(target int) {
+	if head == nil {
+		panic("")
+	}
+
+	var node *SNode = head
+	var prev *SNode = nil
+outer:
+	for {
+		if (*node).get() == target {
+			(*prev).setNext((*node).getNext())
+			(*node).clear()
+			break outer
+		}
+
+		if (*node).getNext() == nil {
+			break outer
+		} else {
+			prev = node
+			node = (*node).getNext()
+		}
+	}
+}
+
+func Singly() {
+	head = MakeNewNode(0)
+	for index := 1; index < 11; index++ {
+		MakeNext(index)
+	}
+
+	Tour(func(x int) { fmt.Println(x) })
+	InsertNext(3, -152125)
+	Delete(4)
+	Tour(func(x int) { fmt.Println(x) })
+	Clear()
+	head.clear()
+	head = nil
+}
--- example-main.go
+++ example-main.go
@@ -4,10 +4,14 @@ import (
 	"time"
 
 	"github.com/rs/zerolog/log"
+
+	. "github.com/yoonhwan/go-example/datastructure"
 )
 
 func main() {
 	logger()
+
+	Singly()
 	test()
 	go contextText()
 	go test_signal()
@@ -17,4 +21,5 @@ func main() {
 			log.Info().Msg("running..")
 		}
 	}
+
 }
